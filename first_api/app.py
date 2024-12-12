8ad3da77fdac7d09d06395e8db24f8591d1fb2ba
Lalit Kumar Meena <mohitmeena18803@gmail.com>
2024-12-12 15:10:34 +0530

    update_api

--- first_api/app.py
+++ first_api/app.py
@@ -46,5 +46,48 @@ def items_list():
             return jsonify(item)  # Return the item as a JSON response
 
     return jsonify({"error": "Item not found"}), 404 
+
+
+
+@app.put('/update_items')
+def update_items():
+    request_data = request.get_json()
+    for i in items:
+        if request_data["name"] == i['name']:
+            i['price'] = request_data['price']
+            return jsonify({"message": 'Item updated successfully'}), 200
+    return jsonify({"message": 'Your request does not match any item'}), 404
+
+
+
+
+# @app.delete("/delete_items")
+# def delete_items():
+#     name = request.args.get('name')  # Get 'name' from from server urls
+    
+#     for item in items:
+#         if item["name"] == name:  # Corrected to match the key in dictionary
+#             items.remove(item)
+#         return {"message" : "Items deletes succesfully "}
+    
+#     return {"error": "Item not found"}, 404 
+
+
+@app.delete("/delete_items")
+def delete_items():
+    name = request.args.get('name')  # Get 'name' from URL query parameters
+
+
+
+    
+    global items
+    for item in items:
+        if item["name"] == name:
+            items = [i for i in items if i["name"] != name]  # Remove item
+            return {"message": "Item deleted successfully"}, 200
+
+    return {"error": "Item not found"}, 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
